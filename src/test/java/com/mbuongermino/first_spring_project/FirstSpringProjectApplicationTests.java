d0393f31e98fa1c2db7d14a89281f8c06a490607
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-06 18:47:06 -0300

    Initial commnit

---
+++ src/test/java/com/mbuongermino/first_spring_project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbuongermino.first_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
