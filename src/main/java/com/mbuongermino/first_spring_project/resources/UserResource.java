77e6422a752b30b64f803311edac2341396908c0
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-07 21:34:44 -0300

    added UserService, components registration and updated controller

--- src/main/java/com/mbuongermino/first_spring_project/resources/UserResource.java
+++ src/main/java/com/mbuongermino/first_spring_project/resources/UserResource.java
@@ -1,18 +1,31 @@
 package com.mbuongermino.first_spring_project.resources;
 
 import com.mbuongermino.first_spring_project.entities.User;
+import com.mbuongermino.first_spring_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = ("/users"))
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> getAll() {
-        User user = new User(1L, "Marcelo", "marcelo@user.com", "55599049", "123456");
-        return ResponseEntity.ok(user);
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
