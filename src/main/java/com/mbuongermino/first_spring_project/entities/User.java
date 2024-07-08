1407cda116fe0c349c87f25c15a5f85f2fde0bc7
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-07 22:20:00 -0300

    created order Order entity, created relationship

--- src/main/java/com/mbuongermino/first_spring_project/entities/User.java
+++ src/main/java/com/mbuongermino/first_spring_project/entities/User.java
@@ -2,69 +2,82 @@ package com.mbuongermino.first_spring_project.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
-    private String Name;
-    private String Email;
-    private String Phone;
-    private String Password;
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
     public User(Long Id, String Name, String Email, String Phone, String Password) {
         super();
-        this.Id = Id;
-        this.Name = Name;
-        this.Email = Email;
-        this.Phone = Phone;
-        this.Password = Password;
+        this.id = Id;
+        this.name = Name;
+        this.email = Email;
+        this.phone = Phone;
+        this.password = Password;
     }
 
     public long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String email) {
-        Email = email;
+        this.email = email;
     }
 
     public String getPhone() {
-        return Phone;
+        return phone;
     }
 
     public void setPhone(String phone) {
-        Phone = phone;
+        this.phone = phone;
     }
 
     public String getPassword() {
-        return Password;
+        return password;
     }
 
     public void setPassword(String password) {
-        Password = password;
+        this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return this.orders;
     }
 
     @Override
@@ -72,11 +85,11 @@ public class User implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Id == user.Id;
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(Id);
+        return Objects.hashCode(id);
     }
 }
