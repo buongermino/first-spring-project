3ef89c0b0ca73221fad8835408e4515cbeed36f4
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-15 19:11:27 -0300

    added many to many between product and category

--- src/main/java/com/mbuongermino/first_spring_project/configs/TestConfig.java
+++ src/main/java/com/mbuongermino/first_spring_project/configs/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User user1 = new User(null, "John Doe", "johndoe@user.com", "9193939393", "123345");
         User user2 = new User(null, "Jane Doe", "janedoe@user.com", "9193939393", "123345");
 
