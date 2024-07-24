8788a18e728cf803a02a3606c6820ad59b05d0f0
Marcelo Buongermino <buon.marcelo@outlook.com>
2024-07-24 15:41:53 -0300

    feature to insert and delete User from database

--- src/main/java/com/mbuongermino/first_spring_project/resources/UserResource.java
+++ src/main/java/com/mbuongermino/first_spring_project/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.mbuongermino.first_spring_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +29,20 @@ public class UserResource {
         User user = userService.findById(id);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
